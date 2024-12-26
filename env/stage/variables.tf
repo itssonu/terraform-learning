c5d51306cb6ac619456203bc43da348c617c0c0b
itssonu <sonu.techbit@outlook.com>
2024-12-27 00:11:43 +0530

    prod and stafe folder added

---
+++ env/stage/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Environment"
+  type        = string
+}
